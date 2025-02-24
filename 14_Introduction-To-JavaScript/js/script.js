023d3d3f7fb0c0a9bf93c1b79d67f037c2bea8fe
ankit74123 <ankit@apie.tech>
2025-02-24 11:45:10 +0530

    Class-14(Introduction to Javascript Completed)

---
+++ 14_Introduction-To-JavaScript/js/script.js
@@ -0,0 +1,35 @@
+// console.log('Hello Ankit');
+
+// console.warn('This is warning');
+
+// console.error('This is error');
+
+// var a = 10;
+// var b = 20;
+// console.log(a, b);
+
+// var a = 20;
+// console.log(a);
+
+// var a = 10;
+// var b = 20;
+// console.log(a%b);
+
+// alert('Good morning')
+// confirm('Are you 18+')
+// prompt('Enter your name');
+
+// alert - for message
+// confirm - to confirm yes/no
+// prompt - write answer
+
+// var username = prompt('Enter your name')
+// console.log(username);
+
+// var a = 10;
+// a--;
+// console.log(a);
+
+// VAR LET CONST
+//  Mocha
+   
